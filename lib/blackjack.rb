a417e4d611e9d4f63eb33f0bd17e075bf0532719
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-27 13:53:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,23 +30,13 @@ def initial_round
   return card_total
 end
 
-def hit?(card_total)
-  prompt_user
-  get_user_input
-  if user_input = "h"
-    card_total += deal_card
-  elsif user_input == "s"
-  card_total
-else
-  invalid_command
-end
-end
-
 def invalid_command
   puts "Please enter a valid command"
   prompt_user
   get_user_input
 end
+
+
 
 #####################################################
 # get every test to pass before coding runner below #
