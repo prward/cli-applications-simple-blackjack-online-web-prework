fc360895ccdeff923d4417d100eca0b3490ec97a
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-27 13:53:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,17 @@ def invalid_command
   get_user_input
 end
 
-
+def hit?(card_total)
+  prompt_user
+  get_user_input
+  if user_input = "h"
+    card_total += deal_card
+  elsif user_input == "s"
+  card_total
+else
+  invalid_command
+end
+end
 
 #####################################################
 # get every test to pass before coding runner below #
